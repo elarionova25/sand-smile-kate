cd96bf9f13b9d7474bfba7c0ccae85a500bb36b1
Ekaterina Larionova <ekaterinalarionova25@gmail.com>
2021-06-23 17:19:18 +0300

    сделаны слайдеры и переключение в блоке Команда

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,37 @@
-console.log('script work!');
+$(document).ready(function() {
+    console.log('script work!');
+
+
+    // делаем переключение по ссылкам в команде
+    $('.command_list a').on('click', function(e) {
+        e.preventDefault();
+
+        let index = $(this).index('.command_list a');
+        console.log(index);
+
+        $('.command_list a').removeClass('active');
+        $(this).addClass('active');
+
+        $('.command_about').removeClass('command_about_visible');
+        $('.command_about').eq(index).addClass('command_about_visible');
+    });
+
+    // Делаем карусель в секции photos
+    $('.js-slider-wrap').each(function(index, sliderWrap) {
+        let slider = $(sliderWrap).find('.js-slider');
+        let btnPrev = $(sliderWrap).find('.js-btn-prev');
+        let btnNext = $(sliderWrap).find('.js-btn-next');
+
+        $(slider).slick({
+            // autoplay: true,
+            dots: true,
+            prevArrow: btnPrev,
+            nextArrow: btnNext,
+        });
+
+        console.log(index);
+        console.log(sliderWrap);
+    });
+
+
+});
